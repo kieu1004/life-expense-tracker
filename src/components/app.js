2b68eab868cf4548ae2dd4556884d47a4396de0d
Nguyen Thi Bich Kieu <109791045+kieu1004@users.noreply.github.com>
2023-11-11 11:19:00 +0700

    refactor: home and css

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,8 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
-
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
+import Home from '../routes/home.js';
 
 const App = () => (
 	<div id="app">
